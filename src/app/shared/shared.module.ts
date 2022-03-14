feebb632290203e1d0cc1ceacf91948191a29e82
Harold Rojas <harolrop14@gmail.com>
2022-03-14 12:56:15 -0500

    componentes del shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    MenuComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
