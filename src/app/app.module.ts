4d451702a534c939b7d4812253403561c89e897a
haroldgys <harold.rojas@gestionysistemas.com>
2023-07-27 11:32:44 -0500

    routerModule y paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    AppRoutingModule,
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
